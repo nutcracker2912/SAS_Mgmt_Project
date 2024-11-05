b7a57fac5f4b06e44c5c638ca762703d7e2f797c
nutcracker2912 <156701263+nutcracker2912@users.noreply.github.com>
2024-11-05 10:58:26 +0100

    Create Database.py
    This is the database for the project

---
+++ Database.py
@@ -0,0 +1,49 @@
+# Updated Employee Database
+fahud_oil_emp = {
+    'Suresh': ['4094', 'Fahud-oil', 'QCI-CIVIL', 'indian', 'TBA', 0, 'date1', 'date2'],
+    'Kashinathan': ['H9265', 'Fahud-oil', 'MDI', 'indian', 'TBA', 0, 'date1', 'date2'],
+    'Moosa': ['502232', 'Fahud-oil', 'QCI-E&I', 'indian', 'Mohammed', 0, 'date1', 'date2'],
+    'Mohammed': ['502964', 'Fahud-oil', 'QCI-E&I', 'indian', 'Moosa', 0, 'date1', 'date2'],
+    'Shambhu': ['3601', 'Fahud-oil', 'QCI-HDPE/LLRTP', 'indian', 'TBA', 1, 'date1', 'date2'],
+    'Bader': ['511413', 'Fahud-oil', 'QAE', 'omani', 'Omar', 0, '2023-09-05', '2023-10-05'],
+    'Omar': ['502657', 'Fahud-oil', 'QAE', 'omani', 'Bader', 1, '2023-09-05', '2023-09-05'],
+    'Prasanna': ['4088', 'Fahud-oil', 'MDI', 'indian', 'TBA', 1, 'date1', 'date2'],
+}
+
+yibal_oil_emp = {
+    'PSN Reddy': ['1501', 'Yibal-oil', 'QAE', 'indian', 'TBA', 1, 'date1', 'date2'],
+    'Kharusi': ['502910', 'Yibal-oil', 'QCI-MECH', 'omani', 'Sajo Puthupully', 0, 'date1', 'date2'],
+    'Mazin': ['501602', 'Yibal-oil', 'QCI-HDPE', 'omani', 'Saleh', 0, 'date1', 'date2'],
+    'Arul': ['3908', 'Yibal-oil', 'QCI-CIVIL', 'indian', 'TBA', 0, 'date1', 'date2'],
+    'Ahmed': ['502756', 'Yibal-oil', 'QCI-E&I', 'omani', 'Muhand', 0, 'date1', 'date2'],
+    'Khamis': ['501445', 'Yibal-oil', 'QCI-LLRTP', 'indian', 'TBA', 0, 'date1', 'date2'],
+    'Sajo Puthupully': ['3596', 'Yibal-oil', 'QCI-MECH', 'indian', 'Kharusi', 0, 'date1', 'date2'],
+    'Saleh': ['501719', 'Yibal-oil', 'QCI-HDPE', 'omani', 'Mazin', 0, 'date1', 'date2'],
+    'Muhand': ['502715', 'Yibal-oil', 'QCI-E&I', 'omani', 'Ahmed', 0, 'date1', 'date2'],
+}
+
+yibal_gas_emp = {
+    'Senthil Vasudevan': ['1501', 'Yibal-gas', 'QAE', 'indian', 'TBA', 1, 'date1', 'date2'],
+    'Manjunatha': ['502910', 'Yibal-gas', 'QCI-MECH', 'omani', 'Sajo Puthupully', 0, 'date1', 'date2'],
+    'Peter Maxy Lobo': ['501602', 'Yibal-gas', 'QCI-HDPE', 'omani', 'Saleh', 0, 'date1', 'date2'],
+    'Anish Mathew': ['502756', 'Yibal-gas', 'QCI-E&I', 'omani', 'Muhand', 0, 'date1', 'date2'],
+    'Dineshan': ['501445', 'Yibal-gas', 'QCI-LLRTP', 'indian', 'TBA', 0, 'date1', 'date2'],
+    'Akram': ['3596', 'Yibal-gas', 'QCI-MECH', 'indian', 'Kharusi', 0, 'date1', 'date2'],
+    'Khalil': ['501719', 'Yibal-gas', 'QCI-HDPE', 'omani', 'Mazin', 0, 'date1', 'date2'],
+    'Wahith': ['3908', 'Yibal-gas', 'QCI-CIVIL', 'indian', 'TBA', 0, 'date1', 'date2'],
+}
+
+kauther_gas_emp = {
+    'Masood': ['1501', 'kauther-gas', 'MDI', 'indian', 'TBA', 1, 'date1', 'date2'],
+    'Gunasekeran': ['502910', 'kauther-gas', 'QCI-COATING', 'omani', 'Omar_kauther', 0, 'date1', 'date2'],
+    'Basvaraju': ['501602', 'kauther-gas', 'QCI-CIVIL', 'omani', 'Abdul Majeed', 1, 'date1', 'date2'],
+    'Omar_kauther': ['3908', 'kauther-gas', 'QCI-COATING', 'indian', 'Gunasekeran', 0, 'date1', 'date2'],
+    'Waleed': ['3908', 'Yibal-oil', 'QCI-E&I', 'indian', 'Yousaf', 0, 'date1', 'date2'],
+    'Abdul Majeed': ['502756', 'Yibal-oil', 'QCI-CIVIL', 'omani', 'Basvaraju', 0, 'date1', 'date2'],
+    'Yousaf': ['501445', 'Yibal-oil', 'QCI-E&I', 'indian', 'Waleed', 0, 'date1', 'date2'],
+}
+
+nizwa_emp = {
+    'Renjith Suresh': ['1501', 'Nizwa', 'QCI-WELDING', 'indian', 'TBA', 0, 'date1', 'date2'],
+    'Zuhaib Zafar Mohammad': ['502910', 'Nizwa', 'QCI-CIVIL', 'omani', 'TBA', 0, 'date1', 'date2'],
+}
